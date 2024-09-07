43dbdebcd77600c3843fd00878a7f2fa4bbc8f57
samharris0509 <samhancock2023@gmail.com>
2024-09-07 14:24:53 -0500

    Add project files.

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FutureValue.Models; // This ensures that the FutureValueModel is referenced
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+
+    }
+}
